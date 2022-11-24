38a0596c210b940d6f9d7b6db0b19db320acecef
Tim Doppenberg <tim.doppenberg@carapaxit.nl>
2022-11-24 15:30:28 +0100

    Adds footer to form

--- pages/pepper-form/index.tsx
+++ pages/pepper-form/index.tsx
@@ -1,58 +1,18 @@
 import classes from "./PepperForm.module.css";
 
 import { NextPage } from "next";
-import React, { FormEvent } from "react";
+import React from "react";
 import FormBody from "../../components/form/field/form-body";
 import TextInput, {
 	TextInputProps,
 } from "../../components/form/field/text-input";
-import {
-	FormItem,
-	FormValueType,
-	InputType,
-} from "../../components/form/interfaces/formvalues.interface";
 import PepperFormValues, { PepperFormFields } from "./pepper-form-values";
 import Button from "../../components/button";
 
-const pepperFormValues2: FormItem<InputType, FormValueType>[] = [
-	{
-		type: "text",
-		name: "Number",
-	},
-	{
-		type: "text",
-		name: "Name",
-	},
-	{
-		type: "text",
-		name: "Color",
-	},
-	{
-		type: "checkbox",
-		name: "Cross",
-	} as FormItem<"checkbox", boolean>,
-	{
-		type: "select",
-		name: "Heat level",
-	} as FormItem<"select">,
-	{
-		type: "text",
-		name: "Years in garden",
-	},
-	{
-		type: "text",
-		name: "Currently growing",
-	},
-	{
-		type: "textarea",
-		name: "Remarks",
-	} as FormItem<"textarea">,
-];
 const PepperForm: NextPage = () => {
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		console.log('WE have a submit!');
-
 	};
 	return (
 		<div className={classes.pepperForm}>
@@ -68,7 +28,7 @@ const PepperForm: NextPage = () => {
 							return <TextInput {...textItem} key={index}></TextInput> ;
 					}
 				})}
-				<Button type="submit">Submit</Button>
+				<footer className={classes.formFooter}><Button type="submit">Submit</Button></footer>
 			</FormBody>
 		</div>
 	);
