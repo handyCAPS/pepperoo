e4bfc973a2a74cd35d5e2033fe0de976c0ef48f7
Tim Doppenberg <tim.doppenberg@carapaxit.nl>
2022-12-10 23:49:04 +0100

    Refines pepperform

--- pages/pepper-form/index.tsx
+++ pages/pepper-form/index.tsx
@@ -14,26 +14,43 @@ import Button from "../../components/button";
 import CheckboxInput, {
 	CheckboxInputProps,
 } from "../../components/form/field/checkbox-input";
+import { Pepper } from "../../interfaces/pepper.interface";
 
-const handleSubmit = (event: React.FormEvent<PepperFormElement>) => {
-	event.preventDefault();
-
-	const {
-		name,
-		color,
-		cross,
-		fNumber,
-		heatLevel,
-		yearsGrowing,
-		remarks,
-		currentlyGrowing,
-	} = event.currentTarget.elements;
-
-	console.log("WE have a submit!");
+export interface PepperFormProps {
+	pepper?: Pepper;
 };
-const PepperForm: NextPage = () => {
+
+const PepperForm: NextPage<PepperFormProps> = (props) => {
+	const handleSubmit = (event: React.FormEvent<PepperFormElement>) => {
+		event.preventDefault();
+
+		const {
+			name,
+			color,
+			cross,
+			fNumber,
+			heatLevel,
+			yearsGrowing,
+			remarks,
+			currentlyGrowing,
+		} = event.currentTarget.elements;
+
+		const pepperValues: Pepper = {
+			id: props.pepper?.id,
+			name: name.value,
+			color: color.value,
+			cross: cross.checked,
+			fNumber: parseInt(fNumber.value) || 0,
+			heatLevel: heatLevel.value,
+			yearsGrowing: parseInt(yearsGrowing.value) || 0,
+			remarks: remarks.value,
+			currentlyGrowing: parseInt(currentlyGrowing.value) || 0,
+		};
+
+		console.log("WE have a submit!", pepperValues);
+	};
 	const formProps: FormProps<PepperFormElement> = {
-		formTitle: "Add Pepper",
+		// formTitle: "Add Pepper",
 		submitHandler: handleSubmit,
 	};
 	return (
@@ -47,7 +64,10 @@ const PepperForm: NextPage = () => {
 					switch (formItem.type) {
 						case "text":
 							const textItem = { ...formItem } as TextInputProps;
-							return <TextInput {...textItem} key={index}></TextInput>;
+							return <TextInput {...textItem} key={index} />;
+						case "textarea":
+							const textAreaItem = { ...formItem, textArea: true } as any;
+							return <TextInput {...textAreaItem} key={index} />;
 						case "checkbox":
 							const checkboxItem = { ...formItem } as CheckboxInputProps;
 							return <CheckboxInput {...checkboxItem} key={index} />;
